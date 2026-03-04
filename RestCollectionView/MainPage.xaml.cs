327707cb3c03d9ddc15238bea0ac0bb38a91a862
itamaratarnity <itamar.atarnity@gmail.com>
2026-03-04 10:45:49 +0200

    Add project files.

---
+++ RestCollectionView/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using RestCollectionView.Models;
+using System.Collections.ObjectModel;
+using System.Text.Json;
+
+namespace RestCollectionView
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnSearchClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                loader.IsVisible = true;
+                loader.IsRunning = true;
+
+                string searchText = searchEntry.Text?.ToLower() ?? "";
+
+                // קריאה לשרת
+                var response = await _httpClient.GetAsync("https://jsonplaceholder.typicode.com/posts");
+
+                response.EnsureSuccessStatusCode();
+
+                var json = await response.Content.ReadAsStringAsync();
+
+                var posts = JsonSerializer.Deserialize<List<Post>>(json);
+
+                // סינון לפי טקסט
+                if (!string.IsNullOrWhiteSpace(searchText))
+                {
+                    posts = posts?.Where(p => p.Title.ToLower()
+                                                    .Contains(searchText))
+                                                    .ToList();
+                }
+                if (posts != null)
+                {
+                    resultsCollection.ItemsSource = new ObservableCollection<Post>(posts); ;
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "OK");
+            }
+            finally
+            {
+                loader.IsRunning = false;
+                loader.IsVisible = false;
+            }
+        }
+    }
+}
---
+++ RestCollectionView/Models/Post.cs
@@ -0,0 +1,19 @@
+﻿namespace RestCollectionView.Models
+{
+    using System.Text.Json.Serialization;
+
+    public class Post
+    {
+        [JsonPropertyName("userId")]
+        public int UserId { get; set; } = 0;
+
+        [JsonPropertyName("id")]
+        public int Id { get; set; } = 0;
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; } = string.Empty;
+
+        [JsonPropertyName("body")]
+        public string Body { get; set; } = string.Empty;
+    } 
+}
